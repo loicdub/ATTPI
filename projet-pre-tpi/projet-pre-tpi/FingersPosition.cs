6fe3bd0ac98c04d6462600171eb3b25045125b56
loicdub <loic.dbsmr@eduge.ch>
2018-04-30 16:23:24 +0200

    AH

--- projet-pre-tpi/projet-pre-tpi/FingersPosition.cs
+++ projet-pre-tpi/projet-pre-tpi/FingersPosition.cs
@@ -6,5 +6,24 @@ using System.Threading.Tasks;
 
 namespace projet_pre_tpi
 {
-    
+    class FingersPosition
+    {
+        #region get-set
+        private string _name;
+        private bool _thumb;
+        private bool _index;
+        private bool _middle;
+        private bool _ring;
+        private bool _pinky;
+
+        public string Name { get => _name; set => _name = value; }
+        public bool Thumb { get => _thumb; set => _thumb = value; }
+        public bool Index { get => _index; set => _index = value; }
+        public bool Middle { get => _middle; set => _middle = value; }
+        public bool Ring { get => _ring; set => _ring = value; }
+        public bool Pinky { get => _pinky; set => _pinky = value; }
+        #endregion
+
+        public FingersPosition() { }
+    }
 }
